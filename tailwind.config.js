06cad1e11f6c4a235ef871211eea7aad9e58671a
Ray <rkwan@skatescribe.com>
2025-03-20 15:09:59 -0400

    use v3 tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 
 const config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
-    "./ui/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/*.{js,ts,jsx,tsx}",
+    "./src/ui/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
