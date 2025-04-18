818316291344a70e65cfc9bd06e09014264fef44
Ray <kwanlokto@gmail.com>
2025-04-18 10:56:11 -0400

    clean up tech stack to be consistent

--- src/ui/tech_stack.tsx
+++ src/ui/tech_stack.tsx
@@ -3,11 +3,16 @@ import { TechStackType, tech_stack } from "@/lib/tech_stack";
 
 export const TechStack = () => {
   return (
-    <Box className="px-6 pt-4 pb-6 border border-gray-300 dark:border-gray-800 rounded-lg">
-      <Typography variant="h6" mb={1}>
+    <Box>
+      <Typography variant="h6" mb={1} pl={3.5}>
         Tech Stack
       </Typography>
-      <Box display="flex" className="flex-wrap" gap={2}>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        className="p-4 border border-gray-300 dark:border-gray-800 rounded-lg"
+        gap={2}
+      >
         {tech_stack.map((tech: TechStackType, index: number) => (
           <Box
             key={index}
