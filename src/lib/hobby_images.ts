a237492b39e02b7ea5d95d2594ffca9890f9922f
Ray Kwan <rkwan@skatescribe.com>
2025-10-28 09:09:44 -0400

    add Antelope canyon

--- src/lib/hobby_images.ts
+++ src/lib/hobby_images.ts
@@ -5,14 +5,16 @@ export type HobbyImageType = {
 
 export const hobby_images: Record<string, HobbyImageType[]> = {
   traveling: [
-    { src: "Lynn Canyon.jpg", aspect_ratio: "3/4" },
-    { src: "Minoh Waterfalls.jpg", aspect_ratio: "9/16" },
+    // Row 1
+    { src: "Antelope Canyon.jpg", aspect_ratio: "3/4" },
     { src: "Mt Fuji.jpg", aspect_ratio: "16/9" },
-    { src: "Osaka Castle.jpg", aspect_ratio: "9/16" },
-    { src: "Rural Japan.jpg", aspect_ratio: "16/9" },
+    { src: "Lynn Canyon.jpg", aspect_ratio: "3/4" },
     { src: "The Crack.jpg", aspect_ratio: "1" },
-    { src: "Vancouver Canucks.jpg", aspect_ratio: "16/9" },
+    // Row 2
+    { src: "Minoh Waterfalls.jpg", aspect_ratio: "9/16" },
+    { src: "Osaka Castle.jpg", aspect_ratio: "3/4" },
     { src: "Whistler.jpg", aspect_ratio: "4/3" },
+    { src: "Vancouver Canucks.jpg", aspect_ratio: "1" },
   ],
   sport: [],
 };
