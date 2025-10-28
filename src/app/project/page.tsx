99e2b2b821981925c64540d5656beec1911318a6
Ray Kwan <rkwan@skatescribe.com>
2025-10-28 09:44:59 -0400

    consistency between pages

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 
 import Projects from "@/ui/projects";
 import React from "react";
@@ -6,9 +6,15 @@ import React from "react";
 export default function Page() {
   return (
     <Box>
-      <Typography mb={2} variant="h5">
+      <Typography
+        variant="h5"
+        fontWeight="bold"
+        gutterBottom
+        sx={{ color: "text.primary" }}
+      >
         My Projects
       </Typography>
+      <Divider sx={{ width: 60, borderBottomWidth: 3, mb: 2 }} />
       <Projects />
     </Box>
   );
