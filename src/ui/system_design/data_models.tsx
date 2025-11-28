9dee0e75655daaf647552e0e75342c81253fbb59
Ray <lokto.kwan@gmail.com>
2025-11-27 19:05:02 -0500

    refactor data model

--- src/ui/system_design/data_models.tsx
+++ src/ui/system_design/data_models.tsx
@@ -9,33 +9,35 @@ interface DataModelsInterface {
 
 export const DataModels = ({ schema_definitions }: DataModelsInterface) => {
   return (
-    <Stack spacing={2}>
+    <Stack spacing={3}>
       {schema_definitions.map(({ title, color, schema }, i) => (
         <Paper
           key={i}
+          elevation={2}
           sx={{
+            p: 3,
             borderRadius: 2,
-            boxShadow: 3,
-            p: 2,
           }}
         >
           <Typography
             variant="subtitle1"
             color={color}
-            sx={{ fontWeight: 700, mb: 1 }}
+            sx={{ fontWeight: 700, mb: 2 }}
           >
             {title}
           </Typography>
 
-          <CodeBlock>
-            <Typography
-              component="pre"
-              variant="body2"
-              sx={{ whiteSpace: "pre-wrap", fontFamily: "monospace" }}
-            >
-              {schema}
-            </Typography>
-          </CodeBlock>
+          <Paper variant="outlined" sx={{ p: 2, borderRadius: 2 }}>
+            <CodeBlock>
+              <Typography
+                component="pre"
+                variant="body2"
+                sx={{ whiteSpace: "pre-wrap", fontFamily: "monospace" }}
+              >
+                {schema}
+              </Typography>
+            </CodeBlock>
+          </Paper>
         </Paper>
       ))}
     </Stack>
