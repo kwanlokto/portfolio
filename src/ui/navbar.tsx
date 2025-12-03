11e27187cceb213959e448e35a750182e78d0f56
Ray <lokto.kwan@gmail.com>
2025-12-02 19:19:21 -0500

    increase padding a bit

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -183,7 +183,7 @@ export const Navbar = ({ toggleTheme }: NavbarProps) => {
           open={drawerOpen}
           onClose={() => setDrawerOpen(false)}
         >
-          <Box sx={{ width: 175, p: 0.5, pt: 2 }}>
+          <Box sx={{ width: 175, p: 0.75, pt: 2.5 }}>
             <List disablePadding>
               {tabs.map((tab) => (
                 <Link key={tab.href} href={tab.href} passHref>
