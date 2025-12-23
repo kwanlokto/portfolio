fffeb5632a807db91c67cb1413c8e7f5bbf19779
Ray Kwan <lokto.kwan@gmail.com>
2025-12-23 12:34:52 -0500

    not so ugly bolding

--- src/ui/bold_text.tsx
+++ src/ui/bold_text.tsx
@@ -4,7 +4,7 @@ export const BoldText = ({ children }: { children: React.ReactNode }) => {
   return (
     <Typography
       component="span"
-      fontWeight="900"
+      fontWeight="700"
       variant="inherit"
       color="text.primary"
     >
